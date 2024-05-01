b2c2c8c8ec9908bcad99f5290ebc7b1281506ff1
flowerheap <flowerheap@163.com>
2024-05-01 19:30:33 +0800

    feat: 🎸 导入文件成功 todo：分析文件内容

--- GoogleTools/simple-chrome-plugin-demo/js/popup.js
+++ GoogleTools/simple-chrome-plugin-demo/js/popup.js
@@ -2,6 +2,21 @@
 let resetBtn = document.getElementById("resetPage");
 let importFile = document.getElementById("importFile");
 
+//接收到文件的回调函数
+importFile.addEventListener('change', (event) => {
+  console.log("接收到的文件", event.target.files);
+  //读取文件列表的第一个文件
+  const file = event.target.files[0];
+  var reader = new FileReader();
+	reader.onload = function(e) {
+		var data = e.target.result;
+		var workbook = XLSX.read(data, {type: 'binary'});
+    console.log(workbook.Sheets.Sheet1);
+		if(callback) callback(workbook);
+	};
+	reader.readAsBinaryString(file);
+})
+
 importBtn.addEventListener('click', () => {
   console.log('导入');
   importFile.click();
