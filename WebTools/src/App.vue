ff10efc27c30a040b0174ac2c779cdf0cce20ca4
minorcell <minorcell6789@gmail.com>
2024-05-07 11:45:06 +0800

    feat: 加入进度条，展示发送进度

--- WebTools/src/App.vue
+++ WebTools/src/App.vue
@@ -3,6 +3,7 @@ import { ref, computed } from 'vue'
 import * as XLSX from 'xlsx'
 import { Delete } from '@element-plus/icons-vue'
 
+const isClick = ref(false)
 const upload = ref(null) // 上传组件实例
 const excelData = ref([]) // Excel数据
 const emailContent = ref('') // 邮件内容
@@ -75,6 +76,7 @@ const sendEmails = async () => {
     ElMessage.warning('请先导入Excel数据')
     return
   }
+  isClick.value = true
   // 由于邮箱服务不可用，这里模拟一下
   for (let i = 0; i < excelData.value.length; i++) {
     const data = excelData.value[i] // data 是一个完整的对象
@@ -101,141 +103,152 @@ const sendEmails = async () => {
       console.error(error)
     }
   }
+  isClick.value = false
   ElMessage.success('邮件发送完成')
 }
 </script>
 <template>
-  <div class="banner">
-    <!-- 头部 -->
-    <h1>CP-EmailTools</h1>
-    <p>
-      Excel转邮件群发工具 | <span>请务必将Excel表格第一列设置为邮箱地址</span>
-    </p>
-    <!-- 上传和清空 -->
-    <div class="main">
-      <el-upload
-        ref="upload"
-        :limit="1"
-        show-file-list="false"
-        accept=".xlsx, .xls"
-        :on-change="FileChange"
-      >
-        <el-button type="primary" class="upload_bt">上传Excel</el-button>
-      </el-upload>
-      <el-button
-        class="delete_bt"
-        style="margin-left: 10px"
-        type="danger"
-        @click="deleteData"
-        >清空</el-button
-      >
-    </div>
-    <!-- 编辑和预览 -->
-    <div class="show">
-      <QuillEditor
-        theme="snow"
-        v-model:content="emailContent"
-        content-type="html"
-      />
-    </div>
-    <!-- 数据展示 -->
-    <el-table
-      class="table"
-      v-loading="loading"
-      border
-      :data="excelData"
-      height="500"
-    >
-      <el-dialog
-        v-model="dialogVisible"
-        width="500"
-        :before-close="handleClose"
-      >
-      </el-dialog>
-      <el-table-column label="状态" width="80" v-if="excelData.length > 0">
-        <template v-slot:default="scope">
-          <div>
-            <el-button
-              v-if="scope.row.state == 0"
-              type="danger"
-              size="small"
-              disabled
-              class="state_bt"
-              >失败</el-button
-            >
-            <el-button
-              v-else-if="scope.row.state == 1"
-              type="warning"
-              size="small"
-              class="state_bt"
-              disabled
-              >未发送</el-button
-            >
-            <el-button
-              v-else
-              type="success"
-              disabled
-              class="state_bt"
-              size="small"
-              >成功</el-button
-            >
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column
-        v-for="key in columns"
-        :key="key"
-        :prop="key"
-        :label="key"
-        min-width="200"
-      >
-        <template v-slot="scope">
-          <el-input v-model="scope.row[key]"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column
-        fixed="right"
-        label="操作"
-        width="70"
-        v-if="excelData.length > 0"
-      >
-        <template #default="scope">
-          <el-button
-            type="danger"
-            size="normal"
-            circle
-            :icon="Delete"
-            plain
-            @click.prevent="deleteRow(scope.$index)"
-          >
-          </el-button>
-        </template>
-      </el-table-column>
-      <template #empty>
-        <el-empty description="暂无数据">
-          <el-upload
-            ref="upload"
-            :limit="1"
-            show-file-list="false"
-            accept=".xlsx, .xls"
-            :on-change="FileChange"
-          >
-            <el-button type="primary" class="upload_bt">上传Excel</el-button>
-          </el-upload>
-        </el-empty>
-      </template>
-    </el-table>
-
-    <el-button type="primary" size="large" class="send_bt" @click="sendEmails"
-      >一键发送</el-button
-    >
-    <div class="footer" style="margin-top: 10px; margin-bottom: 0px">
+  <div class="box">
+    <el-affix :offset="0" v-show="isClick">
+      <el-progress :percentage="50" :show-text="false" />
+    </el-affix>
+    <div class="banner">
+      <!-- 头部 -->
+      <h1>CP-EmailTools</h1>
       <p>
-        Copyright © 2024 EmailTools.Designed by
-        <a href="https://github.com/CodePaintStudio/CP-EmailTools/"
-          >CodePaint</a
-        >
+        Excel转邮件群发工具 | <span>请务必将Excel表格第一列设置为邮箱地址</span>
       </p>
+      <!-- 上传和清空 -->
+      <div class="main">
+        <el-upload
+          ref="upload"
+          :limit="1"
+          show-file-list="false"
+          accept=".xlsx, .xls"
+          :on-change="FileChange"
+        >
+          <el-button type="primary" class="upload_bt">上传Excel</el-button>
+        </el-upload>
+        <el-button
+          class="delete_bt"
+          style="margin-left: 10px"
+          type="danger"
+          @click="deleteData"
+          >清空</el-button
+        >
+      </div>
+      <!-- 编辑和预览 -->
+      <div class="show">
+        <QuillEditor
+          theme="snow"
+          v-model:content="emailContent"
+          content-type="html"
+        />
+      </div>
+      <!-- 数据展示 -->
+      <el-table
+        class="table"
+        v-loading="loading"
+        border
+        :data="excelData"
+        height="500"
+      >
+        <el-dialog
+          v-model="dialogVisible"
+          width="500"
+          :before-close="handleClose"
+        >
+        </el-dialog>
+        <el-table-column label="状态" width="80" v-if="excelData.length > 0">
+          <template v-slot:default="scope">
+            <div>
+              <el-button
+                v-if="scope.row.state == 0"
+                type="danger"
+                size="small"
+                disabled
+                class="state_bt"
+                >失败</el-button
+              >
+              <el-button
+                v-else-if="scope.row.state == 1"
+                type="warning"
+                size="small"
+                class="state_bt"
+                disabled
+                >未发送</el-button
+              >
+              <el-button
+                v-else
+                type="success"
+                disabled
+                class="state_bt"
+                size="small"
+                >成功</el-button
+              >
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-for="key in columns"
+          :key="key"
+          :prop="key"
+          :label="key"
+          min-width="200"
+        >
+          <template v-slot="scope">
+            <el-input v-model="scope.row[key]"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="70"
+          v-if="excelData.length > 0"
+        >
+          <template #default="scope">
+            <el-button
+              type="danger"
+              size="normal"
+              circle
+              :icon="Delete"
+              plain
+              @click.prevent="deleteRow(scope.$index)"
+            >
+            </el-button>
+          </template>
+        </el-table-column>
+        <template #empty>
+          <el-empty description="暂无数据">
+            <el-upload
+              ref="upload"
+              :limit="1"
+              show-file-list="false"
+              accept=".xlsx, .xls"
+              :on-change="FileChange"
+            >
+              <el-button type="primary" class="upload_bt">上传Excel</el-button>
+            </el-upload>
+          </el-empty>
+        </template>
+      </el-table>
+
+      <el-button
+        type="primary"
+        size="large"
+        class="send_bt"
+        @click="sendEmails"
+        :disabled="excelData.length == 0 || isClick"
+        >一键发送</el-button
+      >
+      <div class="footer" style="margin-top: 10px; margin-bottom: 0px">
+        <p>
+          Copyright © 2024 EmailTools.Designed by
+          <a href="https://github.com/CodePaintStudio/CP-EmailTools/"
+            >CodePaint</a
+          >
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -247,6 +260,10 @@ const sendEmails = async () => {
   text-decoration: none;
   margin: 0;
   padding: 0;
+}
+.box {
+  width: 100%;
+  height: 100%;
 }
 .banner {
   width: 60%;
@@ -386,5 +403,9 @@ const sendEmails = async () => {
       rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
     border-radius: 10px;
   }
+}
+.el-progress--line {
+  margin-top: 0;
+  margin-bottom: 10px;
 }
 </style>
