e7cc36b269fddb637a22435173f70ba0cb0fce79
FlowerHeap <flowerheap@qq.com>
2024-06-10 15:10:26 +0800

    feat: 增加注释

--- WebTools/src/utils/request.js
+++ WebTools/src/utils/request.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: FlowerHeap flowerheap@qq.com
+ * @Date: 2024-06-10 15:07:11
+ * @LastEditors: FlowerHeap flowerheap@qq.com
+ * @LastEditTime: 2024-06-10 15:09:52
+ * @FilePath: \CP-EmailTools\WebTools\src\utils\request.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 
@@ -7,6 +15,7 @@ const instance = axios.create({
   baseURL
 })
 
+// 这是我写的代码
 instance.interceptors.response.use((res) => {
   if (res.data.code === 0) {
     return res
